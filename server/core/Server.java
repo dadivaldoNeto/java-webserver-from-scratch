20c281750c7da3da26a1cadd905a23e61776c3cb
Dadivaldo Mendonca <dadmendo@c2r4s3.42luanda.com>
2026-09-08 19:33:22 +0100

    wip

--- server/core/Server.java
+++ server/core/Server.java
@@ -7,24 +7,32 @@ import java.net.ServerSocket;
 public class Server {
 	private final ServerSocket socketImpl;
 
-	public Server(int port) throws IOException{
+	public Server(int port) throws IOException {
 		socketImpl = new ServerSocket(port);
+	}
+
+	private void accept(int i) throws IOException {
+		java.net.Socket socket = socketImpl.accept();
+		System.out.println("Request received | Client #" + (i + 1));
+		InputStream inputStream = socket.getInputStream();
+
+		byte[] data = inputStream.readAllBytes();
+
+		System.out.print("Client Sent: ");
+
+		for (byte b : data)
+			System.out.print((char) b);
+		System.out.println();
 	}
 
 	public void run() {
 		System.out.println("Waiting for connection");
 		try {
-			java.net.Socket socket = socketImpl.accept();
-			System.out.println("Request received");
-			InputStream inputStream = socket.getInputStream();
-
-			byte[] data = inputStream.readAllBytes();
-			
-			System.out.print("Client Sent: ");
-
-			for (byte b : data)
-				System.out.print((char)b);
-			System.out.println();
+			int i = 0;
+			while (i < 2) {
+				accept(i);
+				++i;
+			}
 		} catch (IOException e) {
 			System.err.println("Can't accept connection because".concat(e.getMessage()));
 		}
