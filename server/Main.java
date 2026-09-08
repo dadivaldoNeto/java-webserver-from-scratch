1db0f61965ead4d6eccf8a00258b8bc046b7d45e
Dadivaldo <dadivaldomendonca010@gmail.com>
2026-09-09 00:27:16 +0100

    feat: Add I/O non-blocking sockets

--- server/Main.java
+++ server/Main.java
@@ -1,11 +1,10 @@
 package server;
 
 import java.io.IOException;
-
 import server.core.Server;
 
-public class Main {
 
+public class Main {
 	public static void main(String[] args) {
 		try {
 			Server server = new Server(8080);
--- server/core/Server.java
+++ server/core/Server.java
@@ -2,19 +2,33 @@ package server.core;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.ServerSocket;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.Set;
 
 public class Server {
-	private final ServerSocket socketImpl;
+	private final ServerSocketChannel socketImpl;
+	private  final Selector selector;
 
 	public Server(int port) throws IOException {
-		socketImpl = new ServerSocket(port);
+		socketImpl = ServerSocketChannel.open();
+		selector = Selector.open();
+		
+		InetSocketAddress addr = new InetSocketAddress(port);
+		socketImpl.bind(addr);		
+		socketImpl.configureBlocking(false);
+
+		socketImpl.register(selector, SelectionKey.OP_ACCEPT);
 	}
 
-	private void accept(int i) throws IOException {
-		java.net.Socket socket = socketImpl.accept();
-		System.out.println("Request received | Client #" + (i + 1));
-		InputStream inputStream = socket.getInputStream();
+	private void accept() throws IOException {
+		SocketChannel socket = socketImpl.accept();
+		System.out.println("Request received");
+		InputStream inputStream = socket.socket().getInputStream();
 
 		byte[] data = inputStream.readAllBytes();
 
@@ -26,12 +40,19 @@ public class Server {
 	}
 
 	public void run() {
-		System.out.println("Waiting for connection");
 		try {
-			int i = 0;
-			while (i < 2) {
-				accept(i);
-				++i;
+			while (true) {
+				System.out.println("[Waiting for a request...]");
+				int can_connect = selector.select();
+				if (can_connect == 0)
+					continue ;
+				Set<SelectionKey> keys = selector.selectedKeys();
+				for (var key : keys) {
+					SelectableChannel sock = key.channel();
+					if (key.isAcceptable()) {
+						accept();
+					}
+				}
 			}
 		} catch (IOException e) {
 			System.err.println("Can't accept connection because".concat(e.getMessage()));
