ae935adf6584246717d26dad9ee1cd42cada23eb
Dadivaldo <dadivaldomendonca010@gmail.com>
2026-09-10 00:30:01 +0100

    feat: Server.java

--- server/core/Server.java
+++ server/core/Server.java
@@ -1,12 +1,11 @@
 package server.core;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.nio.channels.ClosedChannelException;
-import java.nio.channels.SelectableChannel;
+import java.text.MessageFormat;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.util.Set;
@@ -14,57 +13,66 @@ import java.util.Set;
 public class Server {
 	private final ServerSocketChannel socketImpl;
 	private final Selector selector;
+	private static final int MAX_BUFFER = 2048;
+	private ByteBuffer buffer;
+
 
 	public Server(int port) throws IOException {
 		socketImpl = ServerSocketChannel.open();
 		selector = Selector.open();
 
-		InetSocketAddress addr = new InetSocketAddress(port);
-		socketImpl.bind(addr);
+		socketImpl.bind(new InetSocketAddress(port));
 		socketImpl.configureBlocking(false);
-
 		socketImpl.register(selector, SelectionKey.OP_ACCEPT);
+		buffer = ByteBuffer.allocate(MAX_BUFFER);
 	}
 
-	private void epoll_ctl(SelectableChannel channel, int ops) {
-		try {
-			channel.configureBlocking(false);
-			channel.register(selector, ops);
+
+	/***
+	 * 
+	 * Read all datas from client socket
+	 * 
+	 ****/
+	private void read(SocketChannel sock) throws IOException {
+		sock.read(buffer);
+		while (buffer.hasRemaining()) {
+			System.out.print(buffer.getChar());
 		}
-		catch(IOException e) {
-			System.err.println("Error: ".concat(e.getMessage()));
-		}
+		System.out.println();
+		buffer.clear();
 	}
 
-	private void accept() throws IOException {
-		SocketChannel socket = socketImpl.accept();
-		if (socket == null)
-			return ;
-		System.out.println("Request received");
-		epoll_ctl(socket, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
-		System.out.print("Client Connected: ");
+	/***
+	 * It will accept connection, and resgister the socket into the Selector object
+	 ***/
+	private void accept(ServerSocketChannel ch) throws IOException {
+		SocketChannel socket = ch.accept();
+		
+		socket.configureBlocking(false);
+		socket.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+		System.out.println(MessageFormat.format("IP: {0} CONNECTED", socket.getLocalAddress() ));
+	
+		read(socket);
+		socket.close();
 	}
 
 	public void run() {
 		try {
+			
 			while (true) {
-
 				System.out.println("[Waiting for a request...]");
-				int can_connect = selector.select(800000L);
-				if (can_connect == 0)
+				if (selector.select(10000L) == 0)
 					continue;
-				
 				Set<SelectionKey> keys = selector.selectedKeys();
-				
 				for (var key : keys) {
-					SelectableChannel sock = key.channel();
-					if (key.isAcceptable())
-						accept();
+					if (key.isAcceptable() ) {
+						if (key.channel() instanceof ServerSocketChannel ch)
+							accept(ch);
+					}
 					if (key.isReadable()) {
-						System.out.println("Can write! kkk"); 
+						System.out.println("Can read input! kkk"); 
 					}
 				}
-
 				selector.selectedKeys().clear();
 			}
 
