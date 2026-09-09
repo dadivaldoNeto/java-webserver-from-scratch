db36f9a648dae1d13b6c0eae1229e993caff0120
Dadivaldo <dadivaldomendonca010@gmail.com>
2026-09-09 11:55:18 +0100

    wip

--- client/Client.java
+++ client/Client.java
@@ -3,9 +3,17 @@ package client;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 	
+	private static String getInput() {
+		Scanner in = new Scanner(System.in);
+		String userInput = in.nextLine();
+		in.close();
+		return userInput;
+	}
+
 	public static void main(String[] args) {
 
 		try {
@@ -14,6 +22,9 @@ public class Client {
 			OutputStream sendData = socket.getOutputStream();
 			sendData.write(args[0].getBytes());
 			System.out.println("Data was sent");
+		
+			//sendData.write(getInput().getBytes());
+		
 			socket.close();
 		}
 		catch(IOException | ArrayIndexOutOfBoundsException e) {
--- server/core/Server.java
+++ server/core/Server.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
@@ -12,50 +13,63 @@ import java.util.Set;
 
 public class Server {
 	private final ServerSocketChannel socketImpl;
-	private  final Selector selector;
+	private final Selector selector;
 
 	public Server(int port) throws IOException {
 		socketImpl = ServerSocketChannel.open();
 		selector = Selector.open();
-		
+
 		InetSocketAddress addr = new InetSocketAddress(port);
-		socketImpl.bind(addr);		
+		socketImpl.bind(addr);
 		socketImpl.configureBlocking(false);
 
 		socketImpl.register(selector, SelectionKey.OP_ACCEPT);
 	}
 
+	private void epoll_ctl(SelectableChannel channel, int ops) {
+		try {
+			channel.configureBlocking(false);
+			channel.register(selector, ops);
+		}
+		catch(IOException e) {
+			System.err.println("Error: ".concat(e.getMessage()));
+		}
+	}
+
 	private void accept() throws IOException {
 		SocketChannel socket = socketImpl.accept();
+		if (socket == null)
+			return ;
 		System.out.println("Request received");
-		InputStream inputStream = socket.socket().getInputStream();
-
-		byte[] data = inputStream.readAllBytes();
-
-		System.out.print("Client Sent: ");
-
-		for (byte b : data)
-			System.out.print((char) b);
-		System.out.println();
+		epoll_ctl(socket, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+		System.out.print("Client Connected: ");
 	}
 
 	public void run() {
 		try {
 			while (true) {
+
 				System.out.println("[Waiting for a request...]");
-				int can_connect = selector.select();
+				int can_connect = selector.select(800000L);
 				if (can_connect == 0)
-					continue ;
+					continue;
+				
 				Set<SelectionKey> keys = selector.selectedKeys();
+				
 				for (var key : keys) {
 					SelectableChannel sock = key.channel();
-					if (key.isAcceptable()) {
+					if (key.isAcceptable())
 						accept();
+					if (key.isReadable()) {
+						System.out.println("Can write! kkk"); 
 					}
 				}
+
+				selector.selectedKeys().clear();
 			}
+
 		} catch (IOException e) {
-			System.err.println("Can't accept connection because".concat(e.getMessage()));
+			System.err.println("Error: ".concat(e.getMessage()));
 		}
 	}
 }
